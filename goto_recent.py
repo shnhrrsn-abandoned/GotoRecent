b0f347a74562c822ff707378d46e5b7e6d59779f
Shaun Harrison <shnhrrsn@gmail.com>
2011-09-23 22:01:16 -0700

    * Reworked the plugin to show recent files at the top, with all files under it. * Refactored code to call on activate instead of deactivate which seems to have fixed numerous bugs of files getting clear.  Moved recent_filed scope into window. instead of self. * Changed the way files are displayed to display full file paths relative to the folder they're in. * Added close event to remove files from the recent list (still available in "all" files, just not at the top of the list) * Added code to add in all open tabs as recent files on first launch * Changes likely break windows support.

--- goto_recent.py
+++ goto_recent.py
@@ -1,36 +1,103 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, threading
 
 class GotoRecentListener(sublime_plugin.EventListener):
-  def on_deactivated(self, view):
+  def on_activated(self, view):
     if view.file_name():
       view.window().run_command("goto_recent", { "file_name": view.file_name() })
+
+  def on_close(self, view):
+    if view.file_name():
+      for window in sublime.windows():
+        window.run_command("goto_recent", { "file_name": view.file_name(), "remove": True })
 
 class GotoRecentCommand(sublime_plugin.WindowCommand):
   def __init__(self, window):
     sublime_plugin.WindowCommand.__init__(self, window)
-    self.recent_files = []
-    self.enabled      = True
 
-  def unshift(self, file_name):
-    item = [os.path.basename(file_name), file_name]
+    try:
+      window.recent_files.size()
+    except AttributeError:
+      window.recent_files = []
+      window.combined_files = []
 
-    for _ in range(self.recent_files.count(item)):
-      self.recent_files.remove(item)
+    # Pre-populate with already opened files
+    for view in window.views():
+      self.add_file(view.file_name())
 
-    self.recent_files.insert(0, item)
+  def relative_file(self, file_name):
+    folders = self.window.folders()[:]
+    folders.sort(key = len)
+    folders.reverse()
+
+    for folder in folders:
+      if file_name.startswith(folder):
+        return os.path.basename(folder) + file_name[len(folder):]
+    return file_name
+
+  def add_file(self, file_name):
+    relative = self.relative_file(file_name)
+
+    if relative in self.window.recent_files:
+      self.window.recent_files.remove(relative)
+
+    self.window.recent_files.insert(0, relative);
+
+  def remove_file(self, file_name):
+    relative = self.relative_file(file_name)
+
+    if relative in self.window.recent_files:
+      self.window.recent_files.remove(relative)
+
+
+  def absolute_file(self, relative_file):
+    folders = self.window.folders()[:]
+    folders.sort(key = len)
+    folders.reverse()
+
+    first_folder = ""
+    index = relative_file.find("/")
+
+    if index >= 0:
+      first_folder = relative_file[0:index]
+
+    for folder in folders:
+      if os.path.basename(folder) == first_folder:
+        return os.path.dirname(folder) + "/" + relative_file
+
+    return relative_file
 
   def selected(self, index):
     if index >= 0:
-      current_file = self.window.active_view().file_name()
-      target_file  = self.recent_files[index][1]
-      self.unshift(current_file)
+      target_file  = self.absolute_file(self.window.combined_files[index])
       self.window.open_file(target_file)
-    self.enabled = True
+      self.run(target_file)
 
-  def run(self, file_name=None):
-    if self.enabled:
-      if file_name:
-        self.unshift(file_name)
+  def run(self, file_name=None, remove=False):
+    if file_name:
+      if remove:
+        self.remove_file(file_name)
       else:
-        self.enabled = False
-        self.window.show_quick_panel(self.recent_files, self.selected)
+        self.add_file(file_name)
+    else:
+      self.show_panel()
+
+  def show_panel(self):
+    self.window.combined_files = []
+
+    for item in self.window.recent_files:
+      self.window.combined_files.append(item)
+
+    active_file = None
+    if len(self.window.combined_files) > 0:
+      active_file = self.absolute_file(self.window.combined_files.pop(0))
+
+    for folder in self.window.folders():
+      for root, subFolders, files in os.walk(folder):
+        for file in files:
+          file_path = self.relative_file(os.path.join(root,file))
+          if active_file != None and file_path == active_file: continue
+          if file_path.find("/.") >= 0 : continue
+          if not file_path in self.window.combined_files:
+            self.window.combined_files.append(file_path)
+
+    self.window.show_quick_panel(self.window.combined_files, self.selected)
